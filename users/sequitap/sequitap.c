94c00ea34c94aa665c8744bda5726e265ff8ea68
Gyozo Gaspar <66yozo@gmail.com>
2024-04-12 12:36:12 +0200

    Add a new line after led indicators

--- users/sequitap/sequitap.c
+++ users/sequitap/sequitap.c
@@ -147,7 +147,7 @@ void oled_render_layer_state(void) {
     led_t led_state = host_keyboard_led_state();
     oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
     oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    oled_write_ln_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
 }
 
 void oled_render_keylog(void) {
