f0cc824d927e06b409cf017bd8545b908b7d9a65
Gyozo Gaspar <66yozo@gmail.com>
2024-05-15 08:08:55 +0200

    Add mouse layer

--- users/sequitap/layers.h
+++ users/sequitap/layers.h
@@ -39,24 +39,27 @@
 #define WHU_NUM 5
 #define WHU_FUN 6
 #define WHU_NPAD 7
+#define WHU_MOUSE 8
 #define WHU_SAFE_RANGE SAFE_RANGE
 
-#define LHU_BASE 8
-#define LHU_LMOD 9 // Left modifiers
-#define LHU_RMOD 10 // Right modifiers
-#define LHU_NAV 11
-#define LHU_NUM 12
-#define LHU_FUN 13
-#define LHU_NPAD 14
+#define LHU_BASE 9
+#define LHU_LMOD 10 // Left modifiers
+#define LHU_RMOD 11 // Right modifiers
+#define LHU_NAV 12
+#define LHU_NUM 13
+#define LHU_FUN 14
+#define LHU_NPAD 15
+#define LHU_MOUSE 16
 #define LHU_SAFE_RANGE (SAFE_RANGE + 450)
 
-#define MHU_BASE 15
-#define MHU_LMOD 16 // Left modifiers
-#define MHU_RMOD 17 // Right modifiers
-#define MHU_NAV 18
-#define MHU_NUM 19
-#define MHU_FUN 20
-#define MHU_NPAD 21
+#define MHU_BASE 17
+#define MHU_LMOD 18 // Left modifiers
+#define MHU_RMOD 19 // Right modifiers
+#define MHU_NAV 20
+#define MHU_NUM 21
+#define MHU_FUN 22
+#define MHU_NPAD 23
+#define MHU_MOUSE 24
 #define MHU_SAFE_RANGE (SAFE_RANGE + 900)
 
 #define ONE_SHOT_LAYERS WHU_LMOD, WHU_RMOD, WHU_NUM, WHU_FUN, LHU_LMOD, LHU_RMOD, LHU_NUM, LHU_FUN, MHU_LMOD, MHU_RMOD, MHU_NUM, MHU_FUN
